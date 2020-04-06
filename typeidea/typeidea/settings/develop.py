013e93ef24823e2ca568291679ec16338b690ff9
yibin9 <992883354@qq.com>
2020-04-06 20:52:51 +0800

    测试

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -13,7 +13,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'typeidea',
-        'USER': 'root',
+        'USER': 'root' ,
         'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306'
