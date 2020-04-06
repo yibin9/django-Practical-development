c090013f7ff06f7ad2588d29da6a204b32790c15
Tedu <992883354@qq.com>
2020-04-06 20:02:11 +0800

    add files

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,21 @@
+from .base import *  # NOQA
+
+# NOQA这个注释的作用是告诉PEP8规范检测工具，这个地方不用检测
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'typeidea',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+    }
+}
